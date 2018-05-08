94175206bdf9216235cd0d9bed0b80498cc186a5
dykim <emotionalcode@gmail.com>
2018-05-08 18:59:58 +0900

    apply changed method name

--- practices/load_data.py
+++ practices/load_data.py
@@ -5,6 +5,6 @@ sys.path.append(os.path.dirname(os.path.abspath(os.path.dirname(__file__))))
 from megaphone_tokenizer import datasets
 
 
-datasets.megaphones.generate_data_files()
+datasets.megaphones.generate_data_files_from_train_data_file()
 # (x_train, y_train), (x_test, y_test) = datasets.megaphones.load_data()
 # print(x_train)
